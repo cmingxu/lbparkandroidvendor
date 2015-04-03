5fb0060f2a2b669ba4117a540fb1deaf63bb85ef
kevin xu <cming.xu@gmail.com>
2015-04-03 14:53:51 +0800

    initial commit

---
+++ app/src/main/java/com/lbpark/lbparkvendorandroid/vo/Lottery.java
@@ -0,0 +1,10 @@
+package com.lbpark.lbparkvendorandroid.vo;
+
+/**
+ * Created by cmingxu on 4/3/15.
+ */
+public class Lottery {
+    public String serialNum;
+    public String openNum;
+    public String state;
+}
---
+++ app/src/main/java/com/lbpark/lbparkvendorandroid/vo/User.java
@@ -0,0 +1,8 @@
+package com.lbpark.lbparkvendorandroid.vo;
+
+/**
+ * Created by cmingxu on 4/3/15.
+ */
+public class User {
+    public String phone;
+}
